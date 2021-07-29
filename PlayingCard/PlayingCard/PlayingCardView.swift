50a2f9dccc1110ffcb688ff4129b69c9b762f7c0
段文菩 <wenpu.duan@ximalaya.com>
2021-07-29 22:31:54 +0800

    创建PlayingCard

---
+++ PlayingCard/PlayingCard/PlayingCardView.swift
@@ -0,0 +1,15 @@
+//
+//  PlayingCardView.swift
+//  PlayingCard
+//
+//  Created by duanwenpu on 2021/7/29.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    override func draw(_ rect: CGRect) {
+    }
+    
+}
--- PlayingCard/PlayingCard/ViewController.swift
+++ PlayingCard/PlayingCard/ViewController.swift
@@ -7,10 +7,19 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
+    
+    var deck = PlayingCardDeck()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        for _ in 1...10 {
+            if let card = deck.draw() {
+                print("\(card)")
+            }
+        }
         // Do any additional setup after loading the view.
     }
 
